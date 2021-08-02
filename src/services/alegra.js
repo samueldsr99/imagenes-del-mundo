80b9f9d9945200901d9feff6b1c06a3ee0863a3e
samueldsr99 <samueldsr8@gmail.com>
2021-08-02 04:09:16 +0200

    feat: Add part of the basic flow on Home

---
+++ src/services/alegra.js
@@ -0,0 +1,36 @@
+/**
+ * Wrapper for https://developer.alegra.com requests
+ */
+import base64 from 'base-64'
+
+const USERNAME = import.meta.env.VITE_USERNAME;
+const PASSWORD = import.meta.env.VITE_PASSWORD;
+const BASE_URL = import.meta.env.VITE_ALEGRA_BASE_URL;
+
+const accessToken = base64.encode(`${USERNAME}:{PASSWORD}`)
+
+const headers = new Headers()
+
+headers.append('Authorization', `Basic ${accessToken}`)
+headers.append('Content-Type', `application/json`)
+
+export const getAllSellers = () => {
+  return fetch(`${BASE_URL}/sellers`, { headers: headers })
+    .then(r => r.json())
+    .then(r => Promise.resolve(r))
+    .catch(e => Promise.reject(e))
+}
+
+export const upvote = (sellerId, votes) => {
+  console.log('votes', votes)
+  return fetch(`${BASE_URL}/sellers/${sellerId}`, {
+    headers: headers,
+    method: 'PUT',
+    body: JSON.stringify({
+      observations: votes
+    })
+  })
+    .then(r => r.json())
+    .then(r => Promise.resolve(r))
+    .catch(e => Promise.reject(e))
+}

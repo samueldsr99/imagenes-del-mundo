11126573bbfa14d534280205f164a9ab3e295d56
samueldsr99 <samueldsr8@gmail.com>
2021-08-03 02:48:09 +0200

    fix: Fix Home route in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import Home from '@/views/Home.vue'
+import Home from '@/views/Home'
 import NotFound from '@/views/404.vue'
 
 const routes = [

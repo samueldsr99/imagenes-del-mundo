f77c03557af7d5f985b25b3334bb4013f898acb8
samueldsr99 <samueldsr8@gmail.com>
2021-08-03 06:36:30 +0200

    feat: Add Invoice component

---
+++ src/components/Invoice/Invoice.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full border rounded-md shadow-xl px-4 py-5">
+    <div class="text-green-500 font-bold text-2xl md:text-4xl mb-10">
+      Factura de Alegra
+    </div>
+    <pre class="text-yellow-500 border py-2 px-2 overflow-x-scroll">{{ data }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object
+  },
+}
+</script>

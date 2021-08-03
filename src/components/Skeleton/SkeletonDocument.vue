057852e7d23971e0578956e68928a981653d8459
samueldsr99 <samueldsr8@gmail.com>
2021-08-03 18:37:39 +0200

    feat: Add document variant to skeleton

---
+++ src/components/Skeleton/SkeletonDocument.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="border border-gray-300 shadow rounded-xl p-4 max-w-sm w-full mx-auto">
+    <div class="animate-pulse flex flex-col justify-between gap-10">
+      <div class="flex-1 space-y-4 py-1">
+        <div class="h-8 mb-8 bg-gray-400 rounded w-3/4"></div>
+        <div class="space-y-4 pb-3">
+          <div class="h-4 bg-gray-400 rounded w-5/6"></div>
+          <div class="h-4 bg-gray-400 rounded w-8/12"></div>
+          <div class="h-4 bg-gray-400 rounded w-11/12"></div>
+        </div>
+        <div class="space-y-4">
+          <div class="h-4 bg-gray-400 rounded w-5/6"></div>
+          <div class="h-4 bg-gray-400 rounded"></div>
+          <div class="h-4 bg-gray-400 rounded w-5/6"></div>
+          <div class="h-4 bg-gray-400 rounded"></div>
+          <div class="h-4 bg-gray-400 rounded w-5/6"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

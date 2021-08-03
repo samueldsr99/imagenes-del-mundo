6dae3cdda9184c14ca5e66a5f0828fa47bd6cb1f
samueldsr99 <samueldsr8@gmail.com>
2021-08-03 19:54:45 +0200

    feat: Add footer component

---
+++ src/components/TheFooter/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="w-full bg-gray-800 py-4">
+    <span class="font-bold flex items-center justify-center gap-2 text-gray-600">
+      <CodeIcon class="w-4 h-4 text-gray-400" />
+      with
+      <HeartIcon class="w-4 h-4 text-gray-400" />
+      in
+      <span class="inline-flex items-center">
+        <img class="w-4 h-4" src="@/assets/logo.png" />ue
+      </span>
+    </span>
+  </div>
+</template>
+
+<script>
+import { CodeIcon } from '@heroicons/vue/outline'
+import { HeartIcon } from '@heroicons/vue/solid'
+export default {
+  components: {
+    CodeIcon,
+    HeartIcon
+  }
+}
+</script>

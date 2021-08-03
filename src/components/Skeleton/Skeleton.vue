9501c4e5c32bb99df6f218b63c77a610bafebf84
samueldsr99 <samueldsr8@gmail.com>
2021-08-03 02:47:12 +0200

    feat: Add Skeleton component

---
+++ src/components/Skeleton/Skeleton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="border border-gray-300 shadow rounded-xl p-4 max-w-sm w-full mx-auto">
+    <div class="animate-pulse flex flex-col justify-between gap-10">
+      <div class="mx-auto rounded-xl bg-gray-400 h-40 w-full"></div>
+      <div class="flex-1 space-y-4 py-1">
+        <div class="h-4 bg-gray-400 rounded w-3/4"></div>
+        <div class="space-y-2">
+          <div class="h-4 bg-gray-400 rounded"></div>
+          <div class="h-4 bg-gray-400 rounded w-5/6"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
